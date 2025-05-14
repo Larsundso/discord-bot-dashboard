8cd25e6faac0cc5b21fcf0df6f58ac4a8039570a
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2025-05-14 12:08:58 +0200

    responsivity improvements

--- apps/website/src/routes/guilds/[guildId]/[channelId]/+page.svelte
+++ apps/website/src/routes/guilds/[guildId]/[channelId]/+page.svelte
@@ -4,6 +4,7 @@
 	import { CacheEvents } from '@discord-bot-dashboard/cache/src/BaseClient/Cluster/Events';
 	import { source } from 'sveltekit-sse';
 	import type { PageServerData } from './$types';
+	import Content from '$lib/components/message/content.svelte';
 
 	const { data: initialData }: { data: PageServerData } = $props();
 	let messages = $derived(initialData.messages);
@@ -33,17 +34,20 @@
 </script>
 
 <div class="flex flex-col-reverse justify-start items-start gap-5 p-5 min-h-full">
-	{#each sortedMessages as message}		<div class="flex flex-row message gap-2 w-full max-w-full">
+	{#each sortedMessages as message}
+		<div class="flex flex-row message gap-2 w-full max-w-full">
 			<img
 				src={message.author?.avatar_url}
 				alt=""
 				class="w-8 h-8 sm:w-10 sm:h-10 rounded-full flex-shrink-0"
 			/>
-			<div class="flex flex-col justify-start items-start max-w-[calc(100%-3rem)] sm:max-w-[calc(100%-3.5rem)] overflow-hidden">
+			<div
+				class="flex flex-col justify-start items-start max-w-[calc(100%-3rem)] sm:max-w-[calc(100%-3.5rem)] overflow-hidden"
+			>
 				<span class="font-medium truncate max-w-full"
 					>{message.member?.nick || message.author?.global_name || message.author?.username}</span
 				>
-				<span class="break-words w-full whitespace-pre-wrap overflow-hidden">{message.content}</span>
+				<Content content={message.content} />
 			</div>
 		</div>
 	{/each}
