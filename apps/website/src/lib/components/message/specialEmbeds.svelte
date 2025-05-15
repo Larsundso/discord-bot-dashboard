a3c9b024c5c7aafa7f752cc7cd4ba53e74d61015
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2025-05-15 11:57:25 +0200

    image embed support

--- apps/website/src/lib/components/message/specialEmbeds.svelte
+++ apps/website/src/lib/components/message/specialEmbeds.svelte
@@ -4,6 +4,7 @@
 	import InfiniteVideo from './infiniteVideo.svelte';
 	import Automoderation from './automoderation.svelte';
 	import type { RGuild } from '$lib/scripts/RTypes';
+	import Attachment from './attachment.svelte';
 
 	const { embed, guild }: { embed: APIEmbed; guild?: RGuild } = $props();
 </script>
@@ -14,6 +15,17 @@
 	<InfiniteVideo src={embed.video!.url!} />
 {:else if embed.type === 'auto_moderation_message'}
 	<Automoderation {embed} {guild} />
+{:else if embed.type === 'image'}
+	<div class="block max-h-inherit w-323px">
+		<a tabindex="-1" aria-hidden="true" href={embed.thumbnail!.url} data-role="img"></a>
+		<div tabindex="0" aria-label="Image" role="button">
+			<img
+				class="block object-cover min-w-full min-h-full max-w-[calc(100%+1px)] my-2"
+				alt=""
+				src={embed.thumbnail!.url}
+			/>
+		</div>
+	</div>
 {:else}
 	<span class="text-danger">
 		Unhandled {embed.type} embed
