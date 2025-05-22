43002749add4ddd66fe4fd393ecca722c65fe196
Larsundso <larsundso3@gmail.com>
2025-05-22 20:01:03 +0200

    style: Adjust layout and sizing for guild display elements

--- apps/website/src/routes/guilds/+page.svelte
+++ apps/website/src/routes/guilds/+page.svelte
@@ -61,18 +61,18 @@
 		>
 	</div>
 
-	<div class="flex flex-row justify-center items-center gap-4 w-full flex-wrap mt-5">
+	<div class="flex flex-row justify-center items-center gap-2 w-full flex-wrap mt-5">
 		{#each guilds.filter((g) => g.name.toLowerCase().includes(query.toLowerCase())) as guild}
 			<a
-				class="w-15% flex flex-col justify-center items-center gap-2 p-2 relative of-hidden
+				class="w-10% flex flex-col justify-center items-center gap-2 relative of-hidden
     hover:scale-105 transition-all duration-100 ease-in-out"
 				href={`/guilds/${guild.id}`}
 			>
 				{#if guild.icon_url}
-					<img src={guild.icon_url} alt="" class="rounded-full p-2 w-120px z-2" />
+					<img src={guild.icon_url} alt="" class="rounded-full p-2 w-60px z-2" />
 				{:else}
 					<div
-						class="bg-main-lighter p-2 rounded-full transition-all duration-300 ease-in-out w-120px h-120px flex justify-center items-center color-alt-text hover:color-main-text text-lg of-hidden z-2"
+						class="bg-main-lighter p-1 rounded-full transition-all duration-300 ease-in-out w-60px h-60px flex justify-center items-center color-alt-text hover:color-main-text text-lg of-hidden z-2"
 					>
 						{guild.name
 							.split(/\s+/g)
