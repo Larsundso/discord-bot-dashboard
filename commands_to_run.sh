45e81d8c3d67ec8f1856a9e93b10321d7611fb3a
Larsundso <larsundso3@gmail.com>
2025-05-23 10:36:12 +0200

    feat: Set up Prometheus configuration and Docker integration

---
+++ commands_to_run.sh
@@ -0,0 +1,5 @@
+# Create the prometheus directory in configs
+mkdir -p ./configs/prometheus
+
+# Move the existing prometheus.yml file into this directory
+mv ./configs/prometheus.yml ./configs/prometheus/
