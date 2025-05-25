96514591746fdb8a98d2e1be3dce53904320b012
Larsundso <larsundso3@gmail.com>
2025-05-25 21:08:28 +0200

    fix: Restore security headers in hooks.server.ts

--- apps/website/src/hooks.server.ts
+++ apps/website/src/hooks.server.ts
@@ -7,15 +7,15 @@ export const handle: Handle = async ({ event, resolve }) => {
 			event.html.replace('%unocss-svelte-scoped.global%', 'unocss_svelte_scoped_global_styles'),
 	});
 
-	// response.headers.append('Content-Security-Policy', "frame-ancestors 'self' https://top.gg;");
-	// response.headers.append(
-	// 	'Strict-Transport-Security',
-	// 	'max-age=31536000; includeSubDomains; preload;',
-	// );
-	// response.headers.append('X-Frame-Options', 'ALLOW-FROM https://top.gg;');
-	// response.headers.append('X-Content-Type-Options', 'nosniff');
-	// response.headers.append('Referrer-Policy', 'strict-origin-when-cross-origin');
-	// response.headers.append('Permissions-Policy', 'camera=(), microphone=(), document-domain=();');
+	response.headers.append('Content-Security-Policy', "frame-ancestors 'self'");
+	response.headers.append(
+		'Strict-Transport-Security',
+		'max-age=31536000; includeSubDomains; preload;',
+	);
+	response.headers.append('X-Frame-Options', 'ALLOW-FROM');
+	response.headers.append('X-Content-Type-Options', 'nosniff');
+	response.headers.append('Referrer-Policy', 'strict-origin-when-cross-origin');
+	response.headers.append('Permissions-Policy', 'camera=(), microphone=(), document-domain=();');
 
 	return response;
 };
