d82ce29e7e75b2d67e28cd7a89d18a1002d26b78
Larsundso <larsundso3@gmail.com>
2025-05-26 12:50:24 +0200

    chore: Remove unused layout server file for cleaner codebase

--- apps/website/src/routes/@me/+layout.server.ts
+++
@@ -1,6 +0,0 @@
-import { redis } from '$lib/server';
-import { redirect } from '@sveltejs/kit';
-import type { APIUser } from 'discord-api-types/v10';
-import type { LayoutServerLoad } from './$types';
-
-export const load: LayoutServerLoad = async (event) => {};
